34be10e1be0f7005d5e6a1ce59b0025779e5c4f1
atgmep <whalebun@gmail.com>
2017-03-14 15:28:54 +0700

    create project

---
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+/**
+ *
+ * @author USER
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("1st line");// 3:27_14:3:2017_1_nghiatt
+        
+    }
+    
+}
